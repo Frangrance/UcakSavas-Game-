07bd048decc97cb347c6230e310f5ed9d756ec01
Kutay Keleş <45495735+Frangrance@users.noreply.github.com>
2019-03-12 23:40:27 +0300

    Add files via upload

---
+++ Uçak Savaş Oyunu/b161210108_projeödevi/form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace b161210108_projeödevi
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
+            this.pb_bizimucak = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_bizimucak)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pb_bizimucak
+            // 
+            this.pb_bizimucak.BackColor = System.Drawing.Color.Transparent;
+            this.pb_bizimucak.Image = ((System.Drawing.Image)(resources.GetObject("pb_bizimucak.Image")));
+            this.pb_bizimucak.Location = new System.Drawing.Point(297, 380);
+            this.pb_bizimucak.Name = "pb_bizimucak";
+            this.pb_bizimucak.Size = new System.Drawing.Size(110, 57);
+            this.pb_bizimucak.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pb_bizimucak.TabIndex = 1;
+            this.pb_bizimucak.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel1.Controls.Add(this.pb_bizimucak);
+            this.panel1.Location = new System.Drawing.Point(-3, 58);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(710, 440);
+            this.panel1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.SteelBlue;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(707, 58);
+            this.label1.TabIndex = 4;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(706, 494);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Uçaksavar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_bizimucak)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pb_bizimucak;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Uçak Savaş Oyunu/b161210108_projeödevi/form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace b161210108_projeödevi
+{
+    public partial class form1 : Form
+    {
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+        int hareketmesafesi = 40;
+        int sayac = 0;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            timer1.Stop();
+           
+            dusman_olustur();
+
+     
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+          if(e.KeyCode==Keys.Enter)
+            {
+                timer1.Start();
+            }
+            
+            if(e.KeyCode==Keys.Left)
+            {
+                if(pb_bizimucak.Location.X-hareketmesafesi+50>0)
+                pb_bizimucak.Location = new System.Drawing.Point(pb_bizimucak.Location.X - hareketmesafesi, pb_bizimucak.Location.Y);
+            }
+            else if(e.KeyCode==Keys.Right)
+            {
+                if (pb_bizimucak.Location.X + hareketmesafesi-50+pb_bizimucak.Width< panel1.Width)
+                    pb_bizimucak.Location = new System.Drawing.Point(pb_bizimucak.Location.X+hareketmesafesi , pb_bizimucak.Location.Y);
+            }
+            if(e.KeyCode==Keys.Space)
+            {
+                mermi_olustur();
+            }
+        }
+        private void mermi_olustur()
+        {
+            PictureBox mermi = new PictureBox();
+            mermi.Image = Image.FromFile("bullet.png");
+            mermi.Width = 20;
+            mermi.Height = 25;
+            mermi.SizeMode = PictureBoxSizeMode.Zoom;
+            mermi.Location = new Point(pb_bizimucak.Location.X + pb_bizimucak.Height, pb_bizimucak.Location.Y - pb_bizimucak.Width/ 2);
+            mermi.BackColor = Color.Transparent;     
+            mermi.Tag = "mermi";
+            panel1.Controls.Add(mermi);
+       }
+       
+        private void dusman_olustur()
+        {
+            Random rast = new Random();
+            PictureBox pic = new PictureBox();
+            pic.Image = Image.FromFile("airplane.png");
+            pic.Width = 150;
+            pic.Height =45;
+            int r = rast.Next(0,panel1.Height+pic.Height+100);
+            pic.SizeMode = PictureBoxSizeMode.Zoom;
+            
+            pic.Location=new Point(r, 0);
+            pic.BackColor = Color.Transparent;
+            pic.Tag = "dusman";
+      
+            panel1.Controls.Add(pic);
+          
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            for (int i=0;i<panel1.Controls.Count;i++)
+            {
+                PictureBox pb = (PictureBox)panel1.Controls[i];
+                if (panel1.Controls[i].Tag == "mermi")
+                {
+                    if (pb.Location.X + pb.Width > panel1.Width)
+                    {
+                        panel1.Controls.RemoveAt(i);
+                    }
+                    else
+                    {
+                        pb.Location = new Point(pb.Location.X, pb.Location.Y-5);
+                    }
+                    for(int j=0;j<panel1.Controls.Count;j++)
+                    {
+                        if(panel1.Controls[j].Tag=="dusman")
+                        {
+                            if(pb.Location.X+pb.Width>panel1.Controls[j].Location.X && pb.Location.X+pb.Width<panel1.Controls[j].Location.X+panel1.Controls[j].Width)
+                            {
+                                if(pb.Location.Y+pb.Height>panel1.Controls[j].Location.Y&&pb.Location.Y+pb.Height<panel1.Controls[j].Location.Y+panel1.Controls[j].Height)
+                                {
+                                    panel1.Controls.RemoveAt(j);
+                                    panel1.Controls.Remove(pb);
+                                }
+                            }
+                        }
+                    }
+                }
+                else if (panel1.Controls[i].Tag == "dusman")
+                {
+                    if (pb.Location.X < 0)
+                    {
+                        panel1.Controls.RemoveAt(i);
+                    }
+                    else
+                    { 
+                        pb.Location = new Point(pb.Location.X, pb.Location.Y + 5);
+                        if(pb.Location.X>panel1.Location.X&&pb.Location.X<panel1.Location.X+panel1.Width)
+                            {
+                            if (pb.Location.Y >= panel1.Location.Y && pb.Location.Y >  panel1.Height || pb.Location.Y >= panel1.Location.Y && pb.Location.Y > pb.Location.Y + pb.Height)
+                            {
+                                panel1.Controls.RemoveAt(i);
+                                timer1.Stop();
+                                MessageBox.Show("OYUN BİTTİ!");
+                               panel1.Controls.Clear();
+                                panel1.Controls.Add(pb_bizimucak);                               
+                                pb.Location = new Point(0, 0);
+                            }
+                        }
+                    }
+                }
+            }
+            if(sayac==15)
+            {
+                sayac = 0;
+                dusman_olustur();
+               
+            }
+       }
+    }
+}
